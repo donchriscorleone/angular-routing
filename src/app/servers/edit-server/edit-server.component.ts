1854bec6ed596eb40062c41e1277feceae846b8f
Christopher II Lajom <christopheriilajom@gmail.com>
2021-08-07 17:50:43 +0800

    Configured handling of query parameters in edit-server component.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: { id: number; name: string; status: string };
   serverName = "";
   serverStatus = "";
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -19,15 +20,19 @@ export class EditServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
-    this.serverName = this.server.name;
-    this.serverStatus = this.server.status;
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams["allowEdit"] === "1" ? true : false;
+    });
+
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params["id"]);
+    });
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {
-      name: this.serverName,
-      status: this.serverStatus,
+      name: this.server.name,
+      status: this.server.status,
     });
   }
 }
