ef924cd5485fdba27718b59df8032f8cacd6e00a
Christopher II Lajom <christopheriilajom@gmail.com>
2021-08-08 13:18:35 +0800

    Added resolver to server component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AuthService } from "./auth.service";
 import { AuthGuardService } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
-import { ErrorPageComponent } from './error-page/error-page.component';
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     AuthGuardService,
     AuthService,
     CanDeactivateGuard,
+    ServerResolver,
   ],
   bootstrap: [AppComponent],
 })
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params, Router } from "@angular/router";
+import { ActivatedRoute, Data, Params, Router } from "@angular/router";
 
 import { ServersService } from "../servers.service";
 
@@ -19,9 +19,12 @@ export class ServerComponent implements OnInit {
 
   ngOnInit() {
     // this.server = this.serversService.getServer(id);
-    this.route.params.subscribe((params: Params) => {
-      const id = +params["id"];
-      this.server = this.serversService.getServer(id);
+    // this.route.params.subscribe((params: Params) => {
+    //   const id = +params["id"];
+    //   this.server = this.serversService.getServer(id);
+    // });
+    this.route.data.subscribe((data: Data) => {
+      this.server = data["server"];
     });
   }
 
